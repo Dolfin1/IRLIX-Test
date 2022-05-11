5d7e38fcc239f5c5d8c856b5eae4eb71dd1aab67
Nikita <nikitatimoshinpost@gmail.com>
2022-05-11 13:56:14 +0300

    изменение валидации

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 
 const form = document.querySelector(".js-form");
 const notepadOutput = document.querySelector(".notepad__output");
+const numberError = document.querySelector(".input__error--number");
+const contentError = document.querySelector(".input__error--content");
 const data = [];
+
+form.addEventListener("input", function (event) {
+	numberError.innerHTML = "";
+	contentError.innerHTML = "";
+});
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
-	const number = this.elements["number"].value;
-	if(number === ""){
-      alert("Заполните номер записи, пожалуйста")
-      return}
-	const content = this.elements["content"].value;
-   if(content === ""){
-      alert("Заполните название записи, пожалуйста")
-      return}
-	data.push({ number, content });
+	const number = this.elements["number"];
+	const content = this.elements["content"];
+	if(!number.value){
+		numberError.innerHTML = "Введите число";
+		return;
+	} else if(!+number.value) {
+		numberError.innerHTML = "Это не число";
+		return;
+	} else {
+		numberError.innerHTML = "";
+	}
+	if(!content.value){
+		contentError.innerHTML = "Введите текст";
+		return;
+	} else {
+		contentError.innerHTML = "";
+	}
+	data.push({ number:number.value, content:content.value });
   	data.sort((a, b) => a.number - b.number);
  	dataCreator();
   	this.reset();
