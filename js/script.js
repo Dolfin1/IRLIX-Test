12f18144b9395ab9a5326c2c53f072d6c1c26a3b
Nikita <nikitatimoshinpost@gmail.com>
2022-05-15 20:04:34 +0300

    Все

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const form = document.querySelector(".js-form");
 const notepadOutput = document.querySelector(".notepad__output");
 const numberError = document.querySelector(".input__error--number");
 const contentError = document.querySelector(".input__error--content");
+const btn = document.querySelector(".btn");
 const data = [];
-
 form.addEventListener("input", function (event) {
 	numberError.innerHTML = "";
 	contentError.innerHTML = "";
@@ -14,30 +14,31 @@ form.addEventListener("submit", function (event) {
 	event.preventDefault();
 	const number = this.elements["number"];
 	const content = this.elements["content"];
-	if(!number.value){
+
+	if (!number.value) {
 		numberError.innerHTML = "Введите число";
 		return;
-	} else if(!+number.value) {
+	} else if (!+number.value) {
 		numberError.innerHTML = "Это не число";
 		return;
 	} else {
 		numberError.innerHTML = "";
 	}
-	if(!content.value){
+	if (!content.value) {
 		contentError.innerHTML = "Введите текст";
 		return;
 	} else {
 		contentError.innerHTML = "";
 	}
-	data.push({ number:number.value, content:content.value });
-  	data.sort((a, b) => a.number - b.number);
- 	dataCreator();
-  	this.reset();
+	data.push({ number: number.value, content: content.value });
+	data.sort((a, b) => a.number - b.number);
+	dataCreator();
+	this.reset();
 });
 
 function dataCreator() {
 	const list = data.map((note) => {
-	return `<div>${note.number}) ${note.content}</div>`;
+		return `<div>${note.number}) ${note.content}</div>`;
 	});
 	notepadOutput.innerHTML = list.join("");
 }
